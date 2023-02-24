a70267a909c17261e07e058ebc0f24e1f732ff5c
Lars Kastner <lars.kastner@posteo.de>
2023-02-24 13:42:21 +0100

    Initial tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using LocalHilbertBurch
+using Oscar
 using Test
 
 @testset "LocalHilbertBurch.jl" begin
-    # Write your tests here.
+    @test U_matrix([1,1,5]) == matrix(ZZ, [1 0 3; 1 0 3; 2 1 4; -1 -2 1])
+    @test order_bounds([7]) == transpose(matrix(ZZ, [8; 1]'))
+    @test degree_bounds([1,2,4]) == matrix(ZZ, [0 0 0; 0 0 0; 0 0 1; 0 0 1])
 end
