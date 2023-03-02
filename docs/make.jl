7f2789aece1233c0843c9d11db06dac44ae46a5f
Anna-Lena Winz <annalena.winz@googlemail.com>
2023-03-02 14:07:56 +0100

    Changed lkastner to anelanna

--- docs/make.jl
+++ docs/make.jl
@@ -11,5 +11,5 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/lkastner/LocalHilbertBurch.jl.git"
+    repo = "github.com/anelanna/LocalHilbertBurch.jl.git"
 )
