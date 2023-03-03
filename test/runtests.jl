7e542f27b270b97e90f33a9478631cdd5523e5af
Lars Kastner <lars.kastner@posteo.de>
2023-03-03 14:40:43 +0100

    Untangle and fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,25 +9,32 @@ using Test
         @test degree_bounds([1,2,4]) == matrix(ZZ, [0 0 0; 0 0 0; 0 0 1; 0 0 1])
     end
 
-    @testset "cells" begin
+    @testset "sorted_celllist" begin
         list = Dict{Int64, Dict{Int64, Vector{Cell}}}()
         for n in 1:5
             list[n] = sorted_celllist(n)
         end
         @test length(list) == 5
         @test [length(list[i]) for i in 1:5] == [1,2,3,4,5]
+    end
+
+    @testset "C1" begin
         R1,(x,y,c...) = PolynomialRing(QQ, [["x","y"];["c["*string(i)*"]" for i in 1:23]])
-        Q,(x,y) = GradedPolynomialRing(QQ, ["x", "y"])
+        Q,(xg,yg) = GradedPolynomialRing(QQ, ["xg", "yg"])
         C1 = Cell([1,5,8,10],R1,Q)
         @test C1.hilb == [1,2,3,4,3,3,3,2,2,1]
-        @test C1.U == matrix(ZZ,[1    4   6   7; 1    4   6   7;-2    1   3   4;-4   -1   1   2;-5   -2   0   1])
-        @test C1.H == matrix(Q,[y     0     0     0;-x   y^4     0     0; 0    -x   y^3     0; 0     0    -x   y^2; 0     0     0    -x])
+        @test C1.U == matrix(ZZ, [1 4 6 7; 1 4 6 7; -2 1 3 4; -4 -1 1 2; -5 -2 0 1])
+        @test C1.H == matrix(Q,[yg 0 0 0; -xg yg^4 0 0; 0 -xg yg^3 0; 0 0 -xg yg^2; 0 0 0 -xg])
         @test C1.M == matrix(R1, [y 0 0  0;   -x y^4 0 0; c[1] -x + y^3*c[4] + y^2*c[3] + y*c[2] y^3 0; c[5] y^3*c[9] + y^2*c[8] + y*c[7] + c[6] -x + y^2*c[11] + y*c[10] y^2; c[12]   y^3*c[16] + y^2*c[15] + y*c[14] + c[13]   y^2*c[19] + y*c[18] + c[17]   -x + y*c[20]])
+    end
+
+    @testset "C2" begin
         R2,(x,y,c...) = PolynomialRing(QQ, [["x","y"];["c["*string(i)*"]" for i in 1:17]])
+        Q,(xg,yg) = GradedPolynomialRing(QQ, ["xg", "yg"])
         C2 = Cell([2,3,5,7],R2,Q)
         @test C2.hilb == [1, 2, 3, 4, 4, 2, 1]
-        @test C2.H == matrix(Q,[y^2    0     0     0; -x    y     0     0;  0   -x   y^2     0;  0    0    -x   y^2; 0    0     0    -x])
-        @test C2.U == matrix(ZZ, [ 2    2   3   4; 1    1   2   3; 1    1   2   3; 0    0   1   2;-1   -1   0   1])
+        @test C2.H == matrix(Q, [yg^2 0 0 0; -xg yg 0 0; 0 -xg yg^2 0; 0 0 -xg yg^2; 0 0 0 -xg])
+        @test C2.U == matrix(ZZ, [2 2 3 4; 1 1 2 3; 1 1 2 3; 0 0 1 2; -1 -1 0 1])
         @test C2.M == matrix(R2, [y^2 0 0 0; -x + y*c[1] y 0 0; y*c[2] -x y^2 0;y*c[4] + c[3] c[5] -x + y*c[6]   y^2; y*c[8] + c[7]   c[9]   y*c[11] + c[10]   -x + y*c[12]])
     end
 end
