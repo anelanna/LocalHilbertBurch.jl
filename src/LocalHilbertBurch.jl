689bcef55507cb78cfe69a58507a9ec5c379a939
Anna-Lena Winz <annalena.winz@googlemail.com>
2023-09-05 14:52:22 +0200

    Too big commit. New structure SmallCell. Homogeneous subcells. Test for differences of dimension of full cell and homogeneous subcell. sorted_celllist_by_HF.

--- src/LocalHilbertBurch.jl
+++ src/LocalHilbertBurch.jl
@@ -109,6 +109,22 @@ function ypolywithcoeffs_matrix(m::Vector{Int64}, v)
     return result, ccount
 end
 
+function homypolywithcoeffs_matrix(m::Vector{Int64}, v)
+    m[1] == 0 || prepend!(m,0)
+    t = length(m)-1
+    U = U_matrix(m)
+    DB = degree_bounds(m)
+    R = parent(v[1])
+    result = zero_matrix(R, t+1, t)
+    ccount = 0
+    for j in 1:t, i in j:t+1 
+        if (0 <= U[i,j] && U[i,j] <= DB[i,j])
+            result[i,j],ccount = ypolywithcoeffs(U[i,j], U[i,j], ccount, v)
+        end
+    end
+    return result, ccount
+end
+
 struct Cell <: CellType
     m::Vector{Int64}
     d::Vector{Int64}
@@ -119,6 +135,9 @@ struct Cell <: CellType
     N::MatElem
     M::MatElem
     dim::Int64
+    N_hom::MatElem
+    M_hom::MatElem
+    dim_hom::Int64
     I
 end
 
@@ -147,30 +166,126 @@ function Cell(m::Vector{Int64}, R, Q)
     N, dim = ypolywithcoeffs_matrix(copy(m), gens(R))
     M = HR+N
     I = ideal(minors(M,ncols(M)))
+    N_hom, dim_hom = homypolywithcoeffs_matrix(copy(m), gens(R))
+    M_hom = HR+N_hom
     hilb = hilbert_function_as_vector(sum(m), Q, E)
-    return Cell(m, d, hilb, HQ, E, U, N, M, dim, I)
+    return Cell(m, d, hilb, HQ, E, U, N, M, dim, N_hom, M_hom, dim_hom, I)
+end
+
+
+function dimm(m::Vector{Int64})
+    m[1] == 0 || prepend!(m,0)
+    t = length(m)-1
+    d = copy(m)
+    d = m_to_d(d)
+    OB = order_bounds(m)
+    N = 0
+    for i in 1:(t+1), j in 1:t
+      if OB[i,j] <= d[min(i,j)]-1
+         N = N + d[min(i,j)] - OB[i,j]
+      end
+    end
+    return N
+end
+
+function dim_homm(m::Vector{Int64})
+    m[1] == 0 || prepend!(m,0)
+    t = length(m)-1
+    d = copy(m)
+    d = m_to_d(d)
+    U = U_matrix(m)
+    DB = degree_bounds(m)
+    N = 0
+    for j in 1:t, i in j+1:t+1
+      if (0 <= U[i,j] && U[i,j] <= DB[i,j])
+         N = N + 1
+      end
+    end
+    return N
 end
 
 function dim(c::CellType)
-    return c.dim
+   return c.dim
 end
 
-function sorted_celllist(n::Int64)
-    result = Dict{Int64, Vector{CellType}}()
+
+function dim_hom(c::CellType)
+   return c.dim_hom
+end
+
+function sorted_celllist(ResultType::Type{T}, n::Int64) where T
+    result = Dict{Int64, Vector{T}}()
     for i in 0:n-1
-        result[i] = Vector{CellType}()
+        result[i] = Vector{T}()
     end
     R,_ = PolynomialRing(QQ, [["x","y"];["c_"*string(i) for i in 1:n]])
     Q,_ = GradedPolynomialRing(QQ, ["x", "y"])
     for partition in Generic.partitions(n)
-        c = Cell(reverse(partition), R, Q)
-        push!(result[dim(c)],c)
+        c = T(reverse(partition), R, Q)
+        push!(result[c.dim],c)
     end
     return result
 end
+sorted_celllist(n::Int) = sorted_celllist(Cell, n)
 
 
-export hilbert_burch_matrix, U_matrix, order_bounds, m_to_d, degree_bounds, hilbert_function_ad_vector, Cell, sorted_celllist, dim
+struct SmallCell
+    m::Vector{Int64}
+    hilb::Vector{fmpq}
+    dim::Int64
+    dim_hom::Int64
+end
+
+
+function SmallCell(m::Vector{Int64}, R, Q)
+    d = copy(m)
+    d = m_to_d(d)
+    HQ = hbm_wrap(d, Q)
+    E = ideal(minors(HQ,ncols(HQ)))
+    U = U_matrix(copy(m))
+    hilb = hilbert_function_as_vector(sum(m), Q, E)
+    dim = dimm(m)
+    dim_hom = dim_homm(m)
+    return SmallCell(m, hilb, dim, dim_hom)
+end
+
+function sorted_celllist_by_HF(ResultType::Type{T}, n::Int64) where T
+    result = Dict{Vector{fmpq}, Vector{T}}()
+    R,_ = PolynomialRing(QQ, [["x","y"];["c_"*string(i) for i in 1:n]])
+    Q,_ = GradedPolynomialRing(QQ, ["x", "y"])
+    for partition in Generic.partitions(n)
+        c = T(reverse(partition), R, Q)
+        hf = c.hilb
+        if !haskey(result, hf)
+            result[hf] = Vector{T}()
+        end
+        push!(result[hf],c)
+    end
+    return result
+end
+sorted_celllist_by_HF(n::Int) = sorted_celllist_by_HF(Cell, n)
+
+function check_dim_diffs(n::Int64)
+   l = sorted_celllist_by_HF(SmallCell, n)
+   for hf in keys(l)
+      selected = l[hf]
+      t = selected[1].dim - selected[1].dim_hom
+      for sc in selected
+         tt = sc.dim - sc.dim_hom
+         if t != tt
+            println(hf)
+            println(sc.m)
+            println(sc.dim)
+            println(sc.dim_hom)
+            println(t)
+            return false
+         end
+      end
+   end
+   return true
+end
+
+export hilbert_burch_matrix, U_matrix, order_bounds, m_to_d, degree_bounds, hilbert_function_ad_vector, Cell, sorted_celllist, sorted_celllist_by_HF, dim, dim_hom, dimm, dim_homm, SmallCell, check_dim_diffs
 include("GradedHilbertBurch.jl")
 
 end
